cd0caa04d7078dfd275754f9d6ab1190b4955cb9
myunusalmeida <muhammadyunusalmeida@gmail.com>
2021-03-08 21:02:22 +0700

    Upload file

---
+++ script/clock.js
@@ -0,0 +1,56 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+
+  var tahun = today.getFullYear();
+  var bulan = today.getMonth();
+  var tanggal = today.getDate();
+  switch (bulan) {
+    case 0:
+      bulan = "Januari";
+      break;
+    case 1:
+      bulan = "Februari";
+      break;
+    case 2:
+      bulan = "Maret";
+      break;
+    case 3:
+      bulan = "April";
+      break;
+    case 4:
+      bulan = "Mei";
+      break;
+    case 5:
+      bulan = "Juni";
+      break;
+    case 6:
+      bulan = "Juli";
+      break;
+    case 7:
+      bulan = "Agustus";
+      break;
+    case 8:
+      bulan = "September";
+      break;
+    case 9:
+      bulan = "Oktober";
+      break;
+    case 10:
+      bulan = "November";
+      break;
+    case 11:
+      bulan = "Desember";
+      break;
+  }
+  document.getElementById("clock").innerHTML = h + ":" + m;
+  document.getElementById("days").innerHTML =
+    tanggal + " " + bulan + " " + tahun;
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
---
+++ script/toggle.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  var toggle = document.getElementById("toggle");
+  var sidebar = document.getElementById("sidebar");
+
+  toggle.classList.toggle("menu-show");
+  sidebar.classList.toggle("menu-show");
+}
